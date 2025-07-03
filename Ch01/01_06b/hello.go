7c4141157448518f311f891bcc4ede150b40cc00
Geoseanlee <lee.zixi.george@outlook.com>
2025-07-03 20:05:46 +1000

    ch1: go build fundamental

--- Ch01/01_06b/hello.go
+++ Ch01/01_06b/hello.go
@@ -7,3 +7,11 @@ import (
 func main() {
 	fmt.Println("Hello from Go!")
 }
+
+//
+//PS E:\Users\leezi\GolandProjects\learning-go-4225046> cd CH01/01_06b
+//PS E:\Users\leezi\GolandProjects\learning-go-4225046\CH01\01_06b> go build hello.go
+//PS E:\Users\leezi\GolandProjects\learning-go-4225046\CH01\01_06b> ./hello
+
+//PS E:\Users\leezi\GolandProjects\learning-go-4225046\CH01\01_06b> go build -o helloworld.exe hello.go
+//PS E:\Users\leezi\GolandProjects\learning-go-4225046\CH01\01_06b> ./helloworld
